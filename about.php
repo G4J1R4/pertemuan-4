9c001ccfd5350510422a82ef7106b89503f96701
G4J1R4 <theokatana0901@gmail.com>
2023-10-13 18:50:32 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Theo</li>
+        <li>Nama Belakang : Nandatama</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kota Salatiga, Kampung Biogas </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kab.Salatiga</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Lari</li>
+        <li>Futsal</li>
+    </ul>
+</section>
